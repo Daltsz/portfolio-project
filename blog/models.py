c552edaba1f74bc2e639f66720419db425b2bd8c
Daltsz <gabrieldadalto@hotmail.com>
2021-02-15 11:30:38 -0300

    Added bootstrap and jobs. It's showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+# Create a Blog Model
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=255)
+    pub_data = models.DateTimeField(auto_now=False, auto_now_add=False)
+    body = models.TextField()
+    image = models.ImageField( upload_to='images/')
+
+
+
+#Title
+#pub_data
+#body
+#image
+
+
+# Add the Blog app to the settings
+
+
+# Create a migration
+
+# Migrate
+
+
+# Add to the Admin
